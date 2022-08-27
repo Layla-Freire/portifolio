40a22f83a39520e2c52c718d99e8aa25e94b8b6d
Layla Freire <94554030+Layla-Freire@users.noreply.github.com>
2022-08-27 12:04:41 -0300

    commit
    modificaçao no envio

--- contato.php
+++ contato.php
@@ -1,18 +1,24 @@
 <?php
-$para = "layla_freire@yahoo.com.br";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
-$mensagem = "<strong>Nome: </strong>".$name;
-$mensagem .= "<br> <strong>Mensagem: </strong>"
-.$_POST['mensagem'];
-$headers =  "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
-$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
-$headers .= "X-Mailer: PHP  v".phpversion()."\n";
-$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headers .= "Return-Path:  <sistema@dominio.com.br>\n";
-$headers .= "MIME-Version: 1.0\n";
+$mensagem = $_POST['mensagem'];
 
-mail($para, $assunto, $mensagem, $headers);
+$assunto  = "Mensagem do site portifólio";
+$seuemail = "layla_freire@yahoo.com.br";
+
+$corpo = "<span style='font-weight: bold'>Nome:</span> $name<br>";
+$corpo .= "Email: $email<br>";
+$corpo .= "Assunto: $assunto<br>";
+$corpo .= "Mensagem: $mensagem<br>";
+
+/* não alterar */
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+mail($seuemail, $assunto, $corpo, $headers);
 ?>
+<script>
+    alert('Dados enviados com sucesso!');
+    location.href="index.html";
+</script>
