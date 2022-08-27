a0a6c0005cc2c059ab839e498b70b4caecee15a3
Layla Freire <94554030+Layla-Freire@users.noreply.github.com>
2022-08-27 12:09:04 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -15,7 +15,7 @@ $corpo .= "Mensagem: $mensagem<br>";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-mail($seuemail, $assunto, $corpo, $headers);
+mail($seuemail, $corpo, $headers);
 ?>
 <script>
     alert('Dados enviados com sucesso!');
